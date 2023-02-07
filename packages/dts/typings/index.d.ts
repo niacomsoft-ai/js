68922b36a50ca5baf895ff38c6888e00a9177463
Wang Yucai <netowls-studio@outlook.com>
2023-02-07 16:04:33 +0800

    feat: 新增 NullableType 类型值。

--- packages/dts/typings/index.d.ts
+++ packages/dts/typings/index.d.ts
@@ -2,3 +2,5 @@
 // LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION. 
 // COPYRIGHT © 2006 - 2023 WANG YUCAI. ALL RIGHTS RESERVED.
 // *******************************************************************************************************************************************************
+
+/// <reference path="./lib.types.d.ts" />
---
+++ packages/dts/typings/lib.types.d.ts
@@ -0,0 +1,25 @@
+// *******************************************************************************************************************************************************
+// LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+// COPYRIGHT © 2006 - 2023 WANG YUCAI. ALL RIGHTS RESERVED.
+// *******************************************************************************************************************************************************
+
+declare namespace sys {
+	/**
+	 * @description 可能为 undefined 的 {@link T} 类型值。
+	 * @author Wang Yucai
+	 *
+	 * @typedef {UndefinedType}
+	 * @template T
+	 */
+	type UndefinedType<T> = T | undefined;
+
+	/**
+	 * @description 可能为 undefined、null 的 {@link T} 类型值。
+	 * @author Wang Yucai
+	 *
+	 * @typedef {NullableType}
+	 * @template T
+	 * @see {@link UndefinedType<T>}
+	 */
+	type NullableType<T> = UndefinedType<T> | null;
+}
