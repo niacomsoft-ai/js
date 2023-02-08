4ae63632fc52cce76de1ac2faa2337597bf65967
Wang Yucai <netowls-studio@outlook.com>
2023-02-08 16:40:59 +0800

    feat: 新增 String 扩展方法。

--- packages/corelib/src/native/string-extensions.ts
+++ packages/corelib/src/native/string-extensions.ts
@@ -3,6 +3,15 @@
 // COPYRIGHT © 2006 - 2023 WANG YUCAI. ALL RIGHTS RESERVED.
 // *******************************************************************************************************************************************************
 
-import "./global-defaults";
-import "./object-extensions";
-import "./string-extensions";
+String.empty = (): string => {
+	return __NIACOMSOFT_EMPTY_STRING__;
+};
+
+String.isNullOrEmpty = (s: sys.NullableType<string>): boolean => {
+	if (Object.isNull(s)) return true;
+	return s === String.empty();
+};
+
+String.isNullOrWhiteSpace = (s: sys.NullableType<string>): boolean => {
+	return String.isNullOrEmpty(s?.trim());
+};
