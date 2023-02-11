9bf8c5f2826dc448276193ce30e165751d977cf7
Wang Yucai <netowls-studio@outlook.com>
2023-02-11 09:44:43 +0800

    refactor: 重构了 CultureInfoFactory 实现方式 Issues #8 。

--- packages/corelib/src/globalization/cultureinfo-factory.ts
+++ packages/corelib/src/globalization/cultureinfo-factory.ts
@@ -6,39 +6,26 @@
 import { sealed } from "../decorators";
 
 /**
- * @description 提供了创建 {@link sys.globalization.ICultureInfo} 相关的方法。密闭的，不可以从此类型派生。
+ * @description 提供了创建 {@link sys.globalization.ICultureInfo} 类型的对象实例相关的方法。密闭的，不可以从此类型派生。
  * @author Wang Yucai
  *
  * @export
  * @class CultureInfoFactory
  * @typedef {CultureInfoFactory}
+ * @implements {sys.globalization.ICultureInfoFactory}
  * @sealed
  */
-
 @sealed
-export class CultureInfoFactory {
+export class CultureInfoFactory implements sys.globalization.ICultureInfoFactory {
 	/**
 	 * 用于初始化一个 {@link CultureInfoFactory} 类型的对象实例。
 	 * @author Wang Yucai
 	 *
 	 * @constructor
-	 * @private
 	 */
-	private constructor() {}
+	constructor() {}
 
-	/**
-	 * @description 创建一个实现了 {@link sys.globalization.ICultureInfo} 类型接口的对象实例。
-	 * @author Wang Yucai
-	 *
-	 * @static
-	 * @param {sys.globalization.ICultureInfoConstructor} factory 工厂方法。
-	 * @param {string} cultureName 文化区域名称。
-	 * @returns {sys.globalization.ICultureInfo}
-	 */
-	static createCulture(
-		factory: sys.globalization.ICultureInfoConstructor,
-		cultureName: string
-	): sys.globalization.ICultureInfo {
+	createCultureInfo(factory: sys.globalization.ICultureInfoConstructor, cultureName: string): sys.globalization.ICultureInfo {
 		return new factory(cultureName);
 	}
 }
--- packages/dts/typings/interfaces/sys.globalization.cultureinfo.d.ts
+++ packages/dts/typings/interfaces/sys.globalization.cultureinfo.d.ts
@@ -38,5 +38,41 @@ declare namespace sys {
 			 */
 			new (cultureName: string): sys.globalization.ICultureInfo;
 		}
+
+		/**
+		 * @description 定义了创建 {@link sys.globalization.ICultureInfo} 类型的对象实例的接口。
+		 * @author Wang Yucai
+		 *
+		 * @interface ICultureInfoFactory
+		 * @typedef {ICultureInfoFactory}
+		 */
+		interface ICultureInfoFactory {
+			/**
+			 * @description 创建名称为 {@link cultureName} 的文化区域信息。
+			 * @author Wang Yucai
+			 *
+			 * @param {sys.globalization.ICultureInfoConstructor} factory 工厂方法。
+			 * @param {string} cultureName 文化区域名称。
+			 * @returns {sys.globalization.ICultureInfo}
+			 */
+			createCultureInfo(
+				factory: sys.globalization.ICultureInfoConstructor,
+				cultureName: string
+			): sys.globalization.ICultureInfo;
+		}
+
+		/**
+		 * @description {@link sys.globalization.ICultureInfoFactory} 工厂方法定义。
+		 * @author Wang Yucai
+		 *
+		 * @interface ICultureInfoFactoryConstructor
+		 * @typedef {ICultureInfoFactoryConstructor}
+		 */
+		interface ICultureInfoFactoryConstructor {
+			/**
+			 * @description 用于初始化一个 {@link sys.globalization.ICultureInfoFactory} 类型的对象实例。
+			 */
+			new (): sys.globalization.ICultureInfoFactory;
+		}
 	}
 }
