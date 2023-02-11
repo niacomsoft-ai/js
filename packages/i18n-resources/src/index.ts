931de361c1bf0b13bafd68f3a294c8811470550b
Wang Yucai <netowls-studio@outlook.com>
2023-02-11 12:27:48 +0800

    feat: 新增多语言资源包项目。

---
+++ packages/i18n-resources/src/index.ts
@@ -0,0 +1,16 @@
+// *******************************************************************************************************************************************************
+// LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.
+// COPYRIGHT © 2006 - 2023 WANG YUCAI. ALL RIGHTS RESERVED.
+// *******************************************************************************************************************************************************
+
+import SimplifiedChineseLanguagePack from "./resources/zh-chs.json";
+import TraditionalChineseLanguagePack from "./resources/zh-cht.json";
+import USALanguagePack from "./resources/en-us.json";
+
+export { SimplifiedChineseLanguagePack, TraditionalChineseLanguagePack, USALanguagePack };
+
+export default {
+	zhCHS: SimplifiedChineseLanguagePack,
+	zhCHT: TraditionalChineseLanguagePack,
+	enUS: USALanguagePack,
+};
